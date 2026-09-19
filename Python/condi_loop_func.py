0f1016410981c8a0790842ad956565e6995d487b
Nahin <nahinworld2024@gmail.com>
2026-09-19 23:46:13 +0600

    conditional statement

---
+++ Python/condi_loop_func.py
@@ -0,0 +1,27 @@
+
+#conditional statement
+
+
+# age = int (input("enter age:"))
+
+# if(age>18):
+#     print ("adult")
+# else:
+#     print ("not adult")   
+
+
+# marks  = int (input("enter marks:"))
+
+# if (marks>=80):
+#     print ("A+")
+# elif(marks>=70):
+#     print ("A")
+# elif(marks>=60):
+#     print("A-")
+# elif(marks>=40):
+#     print("B")
+# elif(marks<40):
+#     print("F")    
+
+
+
--- Python/variable.py
+++ Python/variable.py
@@ -1,4 +1,34 @@
-name = "nahin"
-age = 23
-print (name, age)
+# name = "nahin"
+# age = 23
+# print (name, age)
+
+# print (type(age))
+
+a = 10
+b = 5
+c = b+4
+
+# print (a+b)
+# print (a-b)
+# print (a*b)
+# print (a/b)
+# print (a%b)
+# print (a**b)
+
+# print (a<b)
+# print (not (a<b))
+# print (a+c)
+
+
+# name =  input ("emter name:")
+# print("welcome", name)
+
+a =int( input("enter a:"))
+b = int(input("enter b:"))
+
+print (a+b)
+
+
+
+
 
