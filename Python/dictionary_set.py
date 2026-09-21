2931502cdc457138f470425e982b22db1c8bfaae
Nahin <nahinworld2024@gmail.com>
2026-09-21 08:37:16 +0600

    add dict and sets

---
+++ Python/dictionary_set.py
@@ -0,0 +1,65 @@
+
+
+# info = {
+# "name": "Nahin",
+# "cg": 3.5,
+# "sub":["math","science"],
+
+# }
+
+# print (info)
+# print (info["name"])
+# print(type(info))
+
+# info ["cg"]= 3.9
+# print(info["cg"]) 
+
+# info.update({
+#     "city": "sylhet"
+# })
+
+
+# print (info.get("cg"))
+# print (info.get("cgpa"))
+
+# dict_keys = info.keys()
+# print (dict_keys)
+
+# dict_keys = list(info.keys())
+# print (dict_keys)
+
+# dict_value = info.values()
+# print(dict_value)
+# print(type(dict_value))
+
+# dict_value = list(info.values())
+# print(dict_value)
+# print (type(dict_value))
+
+# print (info.items())
+
+
+
+#sets
+
+
+# s = {1,2,3,3,3}
+
+# print(s)
+
+# s.add(5)
+# s.remove(1)
+# print(s)
+
+# s.pop()
+# print(s)
+
+# s.clear()
+# print(s)
+
+
+s1 ={1,2,3,4,5}
+s2 = {6,7,8,4,5}
+
+print (s1.union(s2))
+print (s1.intersection(s2))
