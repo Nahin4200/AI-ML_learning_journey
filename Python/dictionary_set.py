06dc2dde4e81d5c62c25a0bed341cd66caa1e470
Nahin <nahinworld2024@gmail.com>
2026-09-21 13:15:50 +0600

    add dict and set

--- Python/dictionary_set.py
+++ Python/dictionary_set.py
@@ -58,8 +58,44 @@
 # print(s)
 
 
-s1 ={1,2,3,4,5}
-s2 = {6,7,8,4,5}
+# s1 ={1,2,3,4,5}
+# s2 = {6,7,8,4,5}
 
-print (s1.union(s2))
-print (s1.intersection(s2))
+# print (s1.union(s2))
+# print (s1.intersection(s2))
+
+
+info  = [
+    ("nahin","math"),
+    ("fahiyan","science"),
+    ("nahin","science"),
+    ("fardin","math"),
+    ("fahiyan","math"),
+    ("nahin","english"),
+    ("fardin","english")
+]
+
+uniqe_courses = set()
+
+# for tup in info:
+#     # print(tup[0])
+#     #print (tup[1])
+#     uniqe_courses.add(tup[1])
+# print (uniqe_courses)   
+ 
+ 
+# for name, course in info:
+#     if (course=="english"):
+#         print (name)    
+
+
+dict1 = {}
+for name, course in info:
+    if(dict1.get(name) == None):
+        dict1.update({name: set()})
+        dict1[name].add(course)
+    else:
+        dict1[name].add(course)    
+print(dict1)                     
+
+
