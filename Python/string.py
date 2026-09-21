b5603a77b979906dc9083ca75295446151be2d8e
Nahin <nahinworld2024@gmail.com>
2026-09-21 06:22:12 +0600

    add string

---
+++ Python/string.py
@@ -0,0 +1,29 @@
+
+
+# word1 = "I Love"
+# word2 = " python"
+
+# print (word1+word2)
+# print (len(word1+word2))
+# print (word1[2])
+
+
+# word = "hello"
+# for ch in word:
+#     print(ch)
+
+
+# str1 = "I study python"
+# print (str1[2:7])
+
+
+a = 5 
+b = 10
+sum = a+b
+print ("sum of {} & {} is {}".format(a,b,sum))
+print ("sum of {1} & {0} is {2}".format(a,b,sum))
+print ("language is {}".format ("python"))
+print (f"sum of {a} & {b} is {a+b}")
+
+
+
