fdf2acc7b9bcc42dbc674a0a21314a2944557843
Nahin <nahinworld2024@gmail.com>
2026-09-21 07:03:12 +0600

    add list and tuple

---
+++ Python/list_tuple.py
@@ -0,0 +1,67 @@
+
+# marks = [98,87,86,95,78]
+# print (marks)
+# print (marks [3])
+# print (len(marks))
+# print (marks[1:4])
+
+
+# marks1 = [87,91,78,"nahin",81.56]
+# print (marks1)
+# print(type (marks))
+
+
+# nums = [1,2,3]
+
+# nums.append(4)
+# print (nums)
+
+# nums.insert(2,12)
+# print(nums)
+
+# nums.reverse()
+# print(nums)
+
+# nums.sort()
+# print(nums)
+
+# nums.sort(reverse=True)
+# print (nums)
+
+
+# n1 = [1,2,3,10,4]
+# x = 10
+# idx = 0
+
+# for ch in n1:
+#     if(ch==x):
+#         print (f"{x} found at idx {idx}")
+#         break
+#     idx += 1
+
+
+
+#Tuple
+
+
+# tup = (1,2,3,10,"abc",3.76)
+# print (tup)
+# print (len(tup))
+# print (type(tup))
+# print (tup[4])
+
+
+# tup1 = (1,2,3,4)
+# sum = 0
+# for val in tup1:
+#     sum += val
+# print (sum)    
+   
+
+tup2 = (1,2,2,3,2,4)
+print(tup2.index(3)) 
+print (tup2.count(2))
+
+
+
+
